059a93edd1d4d82dce42ae189e050ae4639b43ea
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-14 17:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+
+  var beatlesPlay = []
+
+  for (i = 0, len = musicians.length; i < len; i++) {
+    beatlesPlay.push(musicians[i] + instruments[i])
+  }
+
+  return beatlesPlay
+}
